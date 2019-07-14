f309466909031ac86d32e7439d5c48bed041e6de
melisa gocay <mgocay@knights.ucf.edu>
2019-07-14 18:28:46 -0400

    End of tutorial

--- Maze/Assets/Scripts/PlayerController.cs
+++ Maze/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody2D rb2d;
     public float speed;
+    public float jumpForce;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,7 +17,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("escape"))
+            Application.Quit();
+
     }
     void FixedUpdate ()
     {
@@ -24,5 +27,16 @@ public class PlayerController : MonoBehaviour
         Vector2 movement = new Vector2 (moveHorizontal, 0);
         rb2d.AddForce(movement * speed);
 
+
+    }
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if(collision.collider.tag == "Ground")
+        {
+            if(Input.GetKey(KeyCode.UpArrow))
+            {
+                rb2d.AddForce(new Vector2(0,jumpForce), ForceMode2D.Impulse);
+            }
+        }
     }
 }
