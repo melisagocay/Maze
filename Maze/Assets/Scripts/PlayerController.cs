75e56f0dea0385507c7a4dca8e6ea0cca137bbbd
melisa gocay <mgocay@knights.ucf.edu>
2019-07-14 18:09:46 -0400

    2nd commit

---
+++ Maze/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis ("Horizontal");
+        Vector2 movement = new Vector2 (moveHorizontal, 0);
+        rb2d.AddForce(movement * speed);
+
+    }
+}
